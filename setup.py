cf8147e23e4d74bd734e702fbfd4bc0049d2e3cc
jwg4 <jwg4@users.noreply.github.com>
2017-11-24 09:11:40 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sample_json',
+    version='0.0.1',
+    description='Samples of valid and invalid JSON',
+    url='https://github.com/jwg4/sample_json',
+    author='Jack Grahl',
+    author_email='jack.grahl@gmail.com',
+    license='Apache 2.0',
+    packages=["sample_json"]
+)
